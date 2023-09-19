b30326e1fefb6ed9a1bafafa7b79696745930ab5
OGTutor <i.send.you.a.letter@gmail.com>
2023-09-19 20:51:19 +0300

    Implemented theme`s hook, context & provider

---
+++ hooks/theme/ThemeProvider.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { FC, useMemo, useState } from 'react';
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+
+interface ThemeProviderProps {
+	children: React.ReactElement;
+}
+
+const defaultTheme =
+	(localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
+
+const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
+	const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+	const toggleChangeTheme = () => {
+		setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+	};
+
+	const defaultProps = useMemo(
+		() => ({
+			theme: theme,
+			setTheme: setTheme,
+		}),
+		[theme]
+	);
+
+	return (
+		<ThemeContext.Provider value={defaultProps}>
+			{children}
+		</ThemeContext.Provider>
+	);
+};
+
+export default ThemeProvider;
