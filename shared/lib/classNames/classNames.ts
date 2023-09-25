ded9682cb30776bd165d74b4247cdc2eb6bba232
OGTutor <i.send.you.a.letter@gmail.com>
2023-09-25 20:00:11 +0300

    Some fixes

--- shared/lib/classNames/classNames.ts
+++ shared/lib/classNames/classNames.ts
@@ -2,16 +2,15 @@ type Mods = Record<string, boolean | string>;
 
 export function classNames(
 	cls: string,
-	mods: Mods,
-	additional: string[]
+	mods: Mods = {},
+	additional: string[] = []
 ): string {
 	return [
 		cls,
-		...additional,
-		Object.entries(mods)
+		...additional.filter(Boolean),
+		...Object.entries(mods)
 			.filter(([className, value]) => Boolean(value))
-			.map(([className, value]) => className)
-			.join(' '),
+			.map(([className, value]) => className),
 	]
 		.join(' ')
 		.trim();
