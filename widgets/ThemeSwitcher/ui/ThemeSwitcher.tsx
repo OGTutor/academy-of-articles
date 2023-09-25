844a2a2c309647c6fea04f3c9daf279b341c6a6b
OGTutor <i.send.you.a.letter@gmail.com>
2023-09-25 19:58:02 +0300

    Implemented a ThemeSwitcher widget

---
+++ widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,62 @@
+import { Theme, useTheme } from '@/app/providers/ThemeProvider';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import Button, { ThemeButton } from '@/shared/ui/Button/Button';
+import Image from 'next/image';
+import { FC } from 'react';
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
+import styles from './ThemeSwitcher.module.scss';
+
+interface ThemeSwitcherProps {
+	theme: string;
+	mode: 'column' | 'row';
+	className?: string;
+}
+
+const ThemeSwitcher: FC<ThemeSwitcherProps> = ({
+	mode = 'row',
+	theme,
+	className = '',
+}) => {
+	const { toggleTheme } = useTheme();
+
+	return (
+		<div
+			className={classNames(
+				'',
+				{
+					[styles.RowMode]: mode === 'row',
+					[styles.ColumnMode]: mode === 'column',
+				},
+				[className]
+			)}
+		>
+			<Image
+				src="/icons/light_dark_icon.svg"
+				alt="Icon"
+				width={50}
+				height={50}
+			/>
+			<span className={styles.icon}>
+				{theme === Theme.DARK ? <BsFillSunFill /> : <BsFillMoonFill />}
+			</span>
+			<span className={styles.text}>
+				{theme === Theme.DARK ? 'Light Mode' : 'Dark Mode'}
+			</span>
+			<Button
+				theme={ThemeButton.CLEAR}
+				className={classNames(
+					styles.ToggleSwitch,
+					{
+						[styles.SwitchDark]: theme === Theme.DARK,
+					},
+					[]
+				)}
+				onClick={toggleTheme}
+			>
+				<span className={styles.switch}></span>
+			</Button>
+		</div>
+	);
+};
+
+export default ThemeSwitcher;
