8ea024cb5d5cc4e589a60092013dad39ef7149d5
OGTutor <i.send.you.a.letter@gmail.com>
2023-11-24 22:14:05 +0200

    Some fixes in i18n

--- shared/config/i18n/i18n.ts
+++ shared/config/i18n/i18n.ts
@@ -1,16 +1,24 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+import translationEN from '@/public/locales/en/translation.json';
+import translationES from '@/public/locales/es/translation.json';
+
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
+
+const resources = {
+	en: { translation: translationEN },
+	es: { translation: translationES },
+};
 
 i18n.use(Backend)
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
+		resources,
 		fallbackLng: 'en',
-		debug: process.env.NODE_ENV === 'development',
-
+		debug: true,
 		interpolation: {
 			escapeValue: false,
 		},
